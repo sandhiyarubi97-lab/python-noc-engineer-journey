98c6092c3c93e8040c62681a782b502c5e47c2a1
sandhiya <sandhiyarubi97@gmail.com>
2026-06-18 21:14:48 +0530

    Day 6: Netmiko SSHautomation working

---
+++ Day06_Netmiko/day6_netmiko.py
@@ -0,0 +1,27 @@
+from netmiko import ConnectHandler
+
+#.........
+cisco_device = {
+    'device_type': 'cisco_ios',  # cisco_ios, juniper, arista_eos etc
+    'host': '192.168.1.1',  # Router/Switch IP
+    'username': 'admin',
+    'password': 'cisco123',
+}
+
+try:
+    print(f"Connecting to {cisco_device['host']}...")
+    net_connect = ConnectHandler(**cisco_device)
+
+    # Command run பண்ணு
+    output = net_connect.send_command("show ip int brief")
+    print(output)
+
+    # File-ல save பண்ணு
+    with open("show_ip_int_brief.txt", "w") as f:
+        f.write(output)
+    print("Output saved to show_ip_int_brief.txt")
+
+    net_connect.disconnect()
+
+except Exception as e:
+    print(f"Connection failed: {e}")
